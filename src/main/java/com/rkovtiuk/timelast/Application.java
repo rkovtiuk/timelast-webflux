8e78441f78b46f7ef21a73efcb9a8e57e6bdf840
Rob Kovtiuk <robsonheater@gmail.com>
2017-12-07 20:27:07 +0200

    Architecture -> Add dependencies

---
+++ src/main/java/com/rkovtiuk/timelast/Application.java
@@ -0,0 +1,13 @@
+package com.rkovtiuk.timelast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@SpringBootApplication
+@EnableWebFlux
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

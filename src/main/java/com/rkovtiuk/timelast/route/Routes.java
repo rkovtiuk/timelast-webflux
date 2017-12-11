804e077760f328534f746958556def628f9044a0
Rob Kovtiuk <robsonheater@gmail.com>
2017-12-12 00:04:06 +0200

    Dependencies -> update spring version

--- src/main/java/com/rkovtiuk/timelast/route/Routes.java
+++ src/main/java/com/rkovtiuk/timelast/route/Routes.java
@@ -1,12 +1,14 @@
-package com.rkovtiuk.timelast.web.route;
+package com.rkovtiuk.timelast.route;
 
 import com.rkovtiuk.timelast.handler.TweetHandler;
+import com.rkovtiuk.timelast.handler.UserHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static com.rkovtiuk.timelast.common.utils.RoutesConstant.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
@@ -14,15 +16,20 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 import static org.springframework.web.reactive.function.server.RequestPredicates.contentType;
 
 @Configuration
-public class TweetRoute {
+public class Routes {
 
     @Bean
-    public RouterFunction<ServerResponse> route(TweetHandler handler) {
+    public RouterFunction<ServerResponse> userRoutes(UserHandler handler) {
         return RouterFunctions
-                .route(GET("/tweets").and(accept(APPLICATION_JSON)), handler::all)
-                .andRoute(GET("/tweets/test").and(accept(APPLICATION_JSON).and(contentType(APPLICATION_JSON))), handler::test)
-                .andRoute(GET("/tweets/{id}").and(accept(APPLICATION_JSON)), handler::one)
-                .andRoute(POST("/tweets").and(accept(APPLICATION_JSON).and(contentType(APPLICATION_JSON))), handler::save);
+                .route(GET(USER).and(accept(APPLICATION_JSON)), handler::one);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> tweetRoutes(TweetHandler handler) {
+        return RouterFunctions
+                .route(GET(ALL_TWEETS).and(accept(APPLICATION_JSON)), handler::all)
+                .andRoute(GET(ONE_TWEET).and(accept(APPLICATION_JSON)), handler::one)
+                .andRoute(POST(ADD_TWEET).and(accept(APPLICATION_JSON).and(contentType(APPLICATION_JSON))), handler::save);
     }
 
 }
--- src/main/java/com/rkovtiuk/timelast/web/controllers/DashboardController.java
+++
@@ -1,7 +0,0 @@
-package com.rkovtiuk.timelast.web.controllers;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class DashboardController {
-}
